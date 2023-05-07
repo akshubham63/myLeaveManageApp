3200d4a8ffd1e4d95db602414b0b3881455e422b
akshubham63 <116741541+akshubham63@users.noreply.github.com>
2023-05-07 14:41:34 +0530

    routings added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { LoginComponent } from './shared/component/account/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '' , redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
